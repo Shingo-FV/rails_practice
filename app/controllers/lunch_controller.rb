f222229a8d856f328e231dc736649b915e8503fd
Shingo Otsu <s1711347@s.tsukuba.ac.jp>
2019-05-10 17:10:25 +0900

    スライド26、27

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,6 +1,6 @@
 class LunchController < ApplicationController
     def go
-        eataries = [ '楽蔵','くぼや','あさひや','天地','鳥大','DALI']
+        eataries = Eatery.all
         @recommend = eataries.sample
     end
 end
